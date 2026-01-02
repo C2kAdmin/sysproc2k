a6f365d541a9ea9e3964ebb06f3b889e94e98d7c
Mikel <conceptocreativoc2k@gmail.com>
2026-01-02 03:53:13 -0300

    Add core instance templates

---
+++ systec/_cores/systec/v1.2/config/instance.example.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * PLANTILLA (sin credenciales).
+ * El sistema debe usar el instance.php de cada CLIENTE (systec/_clients/...).
+ */
+return [
+  'DB_HOST' => 'localhost',
+  'DB_NAME' => 'DATABASE_NAME',
+  'DB_USER' => 'DATABASE_USER',
+  'DB_PASS' => 'DATABASE_PASS',
+];
